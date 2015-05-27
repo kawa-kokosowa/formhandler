9637f578e81a9abfdb140f8f850c9267571181ec
Lillian Lemmer <lillian.lynn.lemmer@gmail.com>
2015-05-27 00:09:56 -0500

    Cleaning up, better documentation, more correct information, README.asciidoc updates.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from distutils.core import setup
 
 
 setup(name='formhandler',
-      version='1.0',
+      version='1.1',
       description='Sometimes the web interface is an afterthought.',
-      author='Lillian Lynn Mahoney',
-      author_email='lillian.lynn.mahoney@gmail.com',
+      author='Lillian Lemmer',
+      author_email='lillian.lynn.lemmer@gmail.com',
       #url='http://www.python.org/sigs/distutils-sig/',
       packages=['formhandler'],
     )
